d056584cfb156dd5a2b9d79659d73d2489e2aef5
Ubuntu <ubuntu@ip-172-31-33-153.us-east-2.compute.internal>
2023-02-05 01:30:35 +0000

    SSH conflicting edit

--- aFile.cpp
+++ aFile.cpp
@@ -2,6 +2,6 @@
 
 int main() {
 	std::cout << "This is a test program." << std::endl;
-	std::cout << "Line2." << std:endl;
+	std::cout << "Line2. Here's a change that will require resolving conflicts." << std:endl;
 	return 0;
 }
